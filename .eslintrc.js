b441580dd562efd5b3b4a115f2935c2c14eba9f0
nathankleung <nathannl808@gmail.com>
2026-09-21 15:26:16 -1000

    2nd attempt finished

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
     'no-restricted-syntax': 'off',
     'no-return-assign': 'off',
     'prefer-arrow-callback': 'off',
+    "@typescript-eslint/no-unused-vars": "error",
   },
 };
